1b74dad91b5d2ddcab343dc95a9844a1c729ace2
Yodi34 <yohan34800@gmail.com>
2021-08-11 14:04:25 +0200

    liste de shopping

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,18 @@
+export class Ingredient{
+// public name : string;
+// public amount : number;
+
+//     constructor(name:string, amount: number)
+//     {
+//         this.name = name;
+//         this.amount = amount;
+//     }
+//- - - - - Deuxieme méthode - - - - - -//
+
+    constructor(public name: string, public amount: number)
+    {
+        
+    }
+
+}
+
