f1a382b6c59bc3effc9847caf2ff6fbf0ed9ac74
Yodi34 <yohan34800@gmail.com>
2021-09-14 15:59:05 +0200

    chapter : databinding & component ok

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
@@ -11,10 +11,18 @@ export class ShoppingListComponent implements OnInit {
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
   ];
-  
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onIngredientAdded(ingredient: Ingredient) {
+    console.log("onIngredientAdded call");
+
+    console.log("ingredient : ", ingredient);
+
+    this.ingredients.push(ingredient);
   }
 
 }
